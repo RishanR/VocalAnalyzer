a64cb43b9c63c12d3d3ce0455e7abac76785454b
RishanRatna <rishanratna@hotmail.com>
2021-01-16 11:34:01 -0500

    React setup complete

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+
+const App = () => {
+    return (
+        <h1>
+            Test
+        </h1>
+    )
+}
+
+export default App;
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
+
